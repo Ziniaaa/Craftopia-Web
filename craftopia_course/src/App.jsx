787aa6ae12fc781412b748c6db6cdd81d33b718e
Ziniaaa <ziniania0727@gmail.com>
2024-12-25 15:01:37 +0800

    1225

--- craftopia_course/src/App.jsx
+++ craftopia_course/src/App.jsx
@@ -4,6 +4,7 @@ import $ from 'jquery'
 import Modal from "./components/Modal"; // 引入 Modal 元件
 import ModalPay from "./components/ModalPay"; // 引入 ModalPay 元件
 import ModalReview from "./components/ModalReview"; // 引入 ModalReview 元件
+import ModalLogIn from "./components/ModalLogIn"; // 引入 ModalLogIn 元件
 import ShowQa from "./components/ShowQa"; // 引入 ShowQa 元件
 import AOS from 'aos';
 import 'aos/dist/aos.css'; //載入node modules中的套件前面不加./
@@ -213,6 +214,17 @@ function App() {
     setIsModalReviewOpen(false);  // 關閉 ModalReview
   };
 
+// 控制 ModalLogIn 開啟狀態
+const [isModalLogInOpen, setIsModalLogInOpen] = useState(false);
+
+const handleOpenModalLogIn = () => {
+  setIsModalLogInOpen(true);  // 開啟 ModalLogIn
+};
+
+const handleCloseModalLogIn = () => {
+  setIsModalLogInOpen(false);  // 關閉 ModalLogIn
+};
+
 
   //AOS淡入淡出套件
   useEffect(() => {
@@ -244,8 +256,13 @@ function App() {
 
       {/*獨立區 */}
       <p className="breadCrumb">首頁＞課程列表＞overloading戒指...</p>
-      <a id="buyNow" onClick={handleOpenModal}><img src="./images/btn-buyNow.svg" alt="" /></a>
+      <button onClick={handleOpenModalLogIn}>會員登入</button>
+      {/* ModalLogIn 元件 */}
+      <ModalLogIn isOpen={isModalLogInOpen} onClose={handleCloseModalLogIn} />
 
+      <a id="buyNow" onClick={handleOpenModal}><img src="./images/btn-buyNow.svg" alt="" /></a>
+{/* ModalLogIn 元件 */}
+<ModalLogIn isOpen={isModalLogInOpen} onClose={handleCloseModalLogIn} />
       <main>
         <section id="intro">
           {/* 課程版頭intro區 */}
@@ -504,11 +521,8 @@ function App() {
                 </div>
               </a>
             </div>
-
-
           </div>
         </section>
-
         <section className="blobstools" >
           {/* 泡泡區 */}
           <figure className="blobs" id="blobs-1" data-aos="fade-up" data-aos-duration="1800"></figure>
@@ -531,82 +545,7 @@ function App() {
 
       {/* Modal_預約彈窗 */}
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <div className="lightBoxWrap">
-          <div className="header">
-            <h3 className="topTitle">選擇預約內容</h3>
-            <button id="iconX" onClick={handleCloseModal}></button>
-            {/* <img src="./images/icons-X.svg" alt="" id="iconX" /> */}
-          </div>
-          <div className="content">
-            <section className="rsvS-1">
-              <div className="rsvcontent">
-                <h3>選擇課程方案</h3>
-                <p className={selectedCourse === "c1" ? "selected" : ""}
-                  onClick={() => handleCourseSelect("c1")}>初階 | 鍛敲戒指</p>
-                <p className={selectedCourse === "c2" ? "selected" : ""}
-                  onClick={() => handleCourseSelect("c2")}>進階 | 造型戒指</p>
-              </div>
-              <hr />
-              <div className="rsvcount">
-                <h3>選擇報名人數</h3>
-                <div>成人 (18歲以上)
-                  <span>
-                    <button id="rsvBtnAdultSub" onClick={() => handleAdultCount("-")}>－</button>
-                    <p id="rsvAdultCount">{adultCount}</p>
-                    <button id="rsvAdultAdd" onClick={() => handleAdultCount("+")}>＋</button>
-                  </span>
-                </div>
-                <div>孩童 (12 ~ 17歲)
-                  <span>
-                    <button id="rsvBtnChildSub" onClick={() => handleChildCount("-")}>－</button>
-                    <p id="rsvChildCount">{childCount}</p>
-                    <button id="rsvChildAdd" onClick={() => handleChildCount("+")}>＋</button>
-                  </span>
-                </div>
-              </div>
-            </section>
-            <section className="rsvDate">
-              <h3>選擇預約日期</h3>
-              <div className="calendar-wrapper">
-                <RsvCalendar setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
-              </div>
-            </section>
-            <section className="rsvS-3">
-              <div className="rsvTime">
-                <h3>選擇課程時段</h3>
-                <ul>
-                  <li className={selectedTime === "t1" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t1")}>10:00~12:00</li>
-                  <li className={selectedTime === "t2" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t2")}>12:00~14:00</li>
-                  <li className={selectedTime === "t3" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t3")}>14:00~16:00</li>
-                  <li className={selectedTime === "t4" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t4")}>16:00~18:00</li>
-                  <li className={selectedTime === "t5" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t5")}>18:00~20:00</li>
-                  <li className={selectedTime === "t6" ? "selected" : ""}
-                    onClick={() => handleTimeSelect("t6")}>20:00~22:00</li>
-                </ul>
-              </div>
-              <div id="FeeNext">
-                <div className="totalFee">金額小計
-                  <span id="rsvTolFee">${totalFee}</span>
-                </div>
-                <button className="nextStep" onClick={handleNextStep}>下一步
-
-                  {/* Tooltip 提示 */}
-                  {showTooltip && (
-                    <div className="toolTip">
-                      <p>每個項目皆需要選取喔!</p>
-                    </div>
-                  )}
-                </button>
-              </div>
-            </section>
-          </div>
-
-        </div>
+      
 
         {/* 付款彈窗 */}
         <ModalPay isOpen={isModalPayOpen} onClose={handleCloseModalPay}>
---
+++ craftopia_course/src/components/ModalLogIn.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import "../scss/_style_ModalLogIn.scss";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa";
+
+
+
+const ModalLogIn = ({ isOpen, onClose }) => {
+
+    if (!isOpen) return null;  // 如果 Modal 沒有開啟，不渲染任何東西
+
+    const [activeTab, setActiveTab] = useState("login"); // 預設是 "會員登入"
+
+    return (
+        <>
+            <div className="ModalLogIn-overlay" onClick={onClose} >
+                    <section id="logInWrap"onClick={(e) => e.stopPropagation()}>
+                        <img src="./images/icons-X.svg" alt="" id="iconX-logIn" onClick={onClose} />
+                        {/* 標籤區塊 */}
+                        <div className="tabs">
+                            <button
+                                className={activeTab === "login" ? "tab active" : "tab"}
+                                onClick={() => setActiveTab("login")}
+                            >
+                                會員登入
+                            </button>
+                            <button
+                                className={activeTab === "register" ? "tab active" : "tab"}
+                                onClick={() => setActiveTab("register")}
+                            >
+                                註冊會員
+                            </button>
+                        </div>
+
+                        {/* 內容區塊 */}
+                        <div className="tab-content">
+                            {activeTab === "login" && (
+                                <div className="login-form">
+                                    <h2>Welcome back!</h2>
+                                    <form>
+                                        <div>
+                                            <label htmlFor="email">Email</label>
+                                            <input type="email" id="email-login" placeholder="請輸入您的 Email" />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="password">密碼</label>
+                                            <input
+                                                type="password"
+                                                id="password-login"
+                                                placeholder="請輸入您的密碼"
+                                            />
+                                        </div>
+                                        <a href="#"><small>忘記密碼</small></a>
+                                        <button type="submit">登入</button>
+                                        <hr />
+                                        <button id="googleLogin"><FcGoogle /> 以 google 繼續</button>
+                                        <button id="fbLogin"><FaFacebookF /> 以 facebook 繼續</button>
+                                    </form>
+                                </div>
+                            )}
+                            {activeTab === "register" && (
+                                <div className="register-form">
+                                    <h2>Welcome on board!</h2>
+                                    <form>
+                                        <div>
+                                            <label htmlFor="name">姓名</label>
+                                            <input type="text" id="name" placeholder="請輸入您的姓名" />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="email">Email</label>
+                                            <input type="email" id="email-register" placeholder="請輸入您的 Email" />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="password">密碼</label>
+                                            <input
+                                                type="password"
+                                                id="password-register"
+                                                placeholder="請輸入您的密碼"
+                                            />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="password">確認密碼</label>
+                                            <input
+                                                type="password"
+                                                id="password-register-confirm"
+                                                placeholder="請再次輸入您的密碼"
+                                            />
+                                        </div>
+                                        <button type="submit">註冊</button>
+                                        <hr />
+                                        <button id="googleLogin-register"><FcGoogle /> 以 google 繼續</button>
+                                        <button id="fbLogin-register"><FaFacebookF /> 以 facebook 繼續</button>
+                                    </form>
+                                </div>
+                            )}
+                        </div>
+                    </section>
+                </div>
+          
+        </>
+
+    );
+};
+
+
+export default ModalLogIn;
